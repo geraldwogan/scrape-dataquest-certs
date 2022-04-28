a5aca0f56cf59bb781034a10172f81420a7ca0a3
geraldwogan <geraldwogan@gmail.com>
2022-04-28 01:50:16 +0100

    Adding webdriver for chrome use with selenium

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
 # Scraping certificates from DataQuest 
+from selenium import webdriver
+
+## Navigate to Dataquest. 
+
+# Selenium Web Driver specifically for chrome, download from here: https://chromedriver.chromium.org/downloads
+driver =  webdriver.Chrome('chromedriver.exe')
+
+
+# Sign-In
+# Navigate to Certificates page: https://app.dataquest.io/profile/gerald.wogan
+# Scrape list of links to certs
+
+# For each cert
+    # Open link
+    # Click Download

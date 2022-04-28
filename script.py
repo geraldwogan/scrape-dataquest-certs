261afd7a200bc6e48c7a728743786ba1fa11d226
geraldwogan <geraldwogan@gmail.com>
2022-04-28 01:55:07 +0100

    Add logic to open certificates webpage

--- script.py
+++ script.py
@@ -1,15 +1,20 @@
 # Scraping certificates from DataQuest 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 ## Navigate to Dataquest. 
 
-# Selenium Web Driver specifically for chrome, download from here: https://chromedriver.chromium.org/downloads
-driver =  webdriver.Chrome('chromedriver.exe')
-
+# Selenium Web Driver specifically for chrome
+driver = webdriver.Chrome(ChromeDriverManager().install())
+# Link to dataquest
+link = "https://app.dataquest.io/profile/gerald.wogan"
+# Open webpage
+driver.get(link)
 
 # Sign-In
 # Navigate to Certificates page: https://app.dataquest.io/profile/gerald.wogan
 # Scrape list of links to certs
+
 
 # For each cert
     # Open link

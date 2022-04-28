925e4269fe96caaee021d2f29b71ab40a2d35775
geraldwogan <geraldwogan@gmail.com>
2022-04-28 19:10:57 +0100

    Scraping links to all certs

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 # Scraping certificates from DataQuest 
+import profile
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 ## Navigate to Dataquest. 
@@ -7,15 +9,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Selenium Web Driver specifically for chrome
 driver = webdriver.Chrome(ChromeDriverManager().install())
 # Link to dataquest
-link = "https://app.dataquest.io/profile/gerald.wogan"
+link_to_profile = "https://app.dataquest.io/profile/gerald.wogan"
 # Open webpage
-driver.get(link)
+driver.get(link_to_profile)
 
-# Sign-In
-# Navigate to Certificates page: https://app.dataquest.io/profile/gerald.wogan
-# Scrape list of links to certs
+## Scrape list of links to certs
+cert_links = []
+# identify elements with tagname <a>
+links = driver.find_elements_by_tag_name("a")
+# traverse list
+for elem in links:
+    # get_attribute() to get all hrefs
+    href = elem.get_attribute('href')
+    if 'view_cert' in str(href):
+        cert_links.append(href)
+print(len(cert_links), 'certificate links found')
 
-
-# For each cert
-    # Open link
-    # Click Download
+## For each cert
+    ## Open link
+    ## Click Download

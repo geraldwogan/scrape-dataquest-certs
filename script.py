2c92ec14f2caed48406f6e9eb1b25d42a393a7fb
geraldwogan <geraldwogan@gmail.com>
2022-04-29 01:11:39 +0100

    Successful end-to-end test for 1 cert!

--- script.py
+++ script.py
@@ -8,37 +8,37 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 ## Navigate to Dataquest. 
 
-# # Selenium Web Driver specifically for chrome
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# # Link to dataquest
-# link_to_profile = "https://app.dataquest.io/profile/gerald.wogan"
-# # Open webpage
-# driver.get(link_to_profile)
+# Selenium Web Driver specifically for chrome
+driver = webdriver.Chrome(ChromeDriverManager().install())
+# Link to dataquest
+link_to_profile = "https://app.dataquest.io/profile/gerald.wogan"
+# Open webpage
+driver.get(link_to_profile)
 
-# ## Scrape list of links to certs
-# cert_links = []
-# # identify elements with tagname <a>
-# links = driver.find_elements(by=By.TAG_NAME, value='a') 
+## Scrape list of links to certs
+cert_links = []
+# identify elements with tagname <a>
+anchors = driver.find_elements(by=By.TAG_NAME, value='a') 
 
-# # traverse list
-# for elem in links:
-#     # get_attribute() to get all hrefs
-#     href = elem.get_attribute('href')
-#     if 'view_cert' in str(href):
-#         cert_links.append(href)
-# certs_found = len(cert_links)
-# print(certs_found, 'certificate links found')
+# traverse list
+for elem in anchors:
+    # get_attribute() to get all hrefs
+    href = elem.get_attribute('href')
+    if 'view_cert' in str(href): 
+        cert_links.append(href)
+certs_found = len(cert_links)
+print(certs_found, 'certificate links found')
 
-## For each cert
+# For each cert
 # for link in cert_links:
 #     ## Open link
 #     driver.get(link)
-# driver.get('https://app.dataquest.io/view_cert/Z4CMFNWE9WXCKKTYMWQE')
-    ## Click Download
-# b = driver.find_elements(by=By.TAG_NAME, value='button') 
-# dl = b[-1]
-# dl.click()
-# time.sleep(1.5)
+driver.get(cert_links[0])
+    # Click Download
+b = driver.find_elements(by=By.TAG_NAME, value='button') 
+dl = b[-1]
+dl.click()
+time.sleep(3)
 
 # Find downloaded certs
 downloads_path = str(Path.home() / "Downloads")
@@ -46,12 +46,15 @@ get_files = []
 for filename in os.listdir(downloads_path):
     if (filename.split('--'))[0] == 'Gerald-Wogan' and (filename.split('.'))[-1] == 'pdf':
         get_files.append(filename)
-print(len(get_files))
+# print(len(get_files))
 
 # Create directory to store certs together
 dq_dir = downloads_path + "\DQ"
 if not os.path.exists(dq_dir):
     os.makedirs(dq_dir)
+    print('Creating directory:', dq_dir)
+else:
+    print('Directory already exists:', dq_dir)
 
 # Move certs to directory
 for g in get_files:
